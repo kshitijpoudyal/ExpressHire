3e63e9e28fef8397de89bde6136ab83fe47bd323
Mohammed <mohammed.ahmed.ps@gmail.com>
2017-08-11 15:43:33 -0500

    commit

--- src/main/java/com/ea/expresshire/controller/RecruiterController.java
+++ src/main/java/com/ea/expresshire/controller/RecruiterController.java
@@ -1,4 +1,7 @@
 package com.ea.expresshire.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RecruiterController {
 }
---
+++ src/main/java/com/ea/expresshire/services/recruiter/RecruiterServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ea.expresshire.services.recruiter;
+
+import com.ea.expresshire.dao.RecruiterRepository;
+import com.ea.expresshire.model.Recruiter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RecruiterServiceImpl implements RecruiterService {
+
+    @Autowired
+    RecruiterRepository recruiterRepository;
+
+    @Override
+    public void addNewRecruiter(Recruiter recruiter) {
+        recruiterRepository.save(recruiter);
+    }
+}
