aa232e066eae6f0cef58c7eb04f0b92e87d9b90d
rawsharma <raw.sharma1379@gmail.com>
2017-08-12 22:41:18 -0500

    job module ip

---
+++ src/main/java/com/ea/expresshire/dao/JobRepository.java
@@ -0,0 +1,12 @@
+package com.ea.expresshire.dao;
+
+import com.ea.expresshire.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Raw on 8/11/2017.
+ */
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
